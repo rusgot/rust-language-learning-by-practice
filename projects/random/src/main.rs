4a4af6669d3d375ce4c3dfe2111ee3c80d723738
rusgot <raymondcasy@gmail.com>
2022-01-18 11:12:52 -0600

    Random testing

--- projects/random/src/main.rs
+++ projects/random/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let s = String::from("hello world");
     let p = String::from("hello world!");
@@ -9,4 +11,13 @@ fn main() {
     let ref_equality = &s == &p;
     println!("Reference equality: {}", ref_equality);
     println!("&s: {}\n&p: {}", &s, &p);
+
+    let s = String::from("abba");
+    let char_vec: Vec<char> = s.chars().collect();
+    println!("{:?}", char_vec);
+
+    let mut v = vec![1, 2, 3, 1];
+    let mut set = HashSet::new();
+    v.retain(|e| set.insert(*e));
+    println!("{:?}", v);
 }
