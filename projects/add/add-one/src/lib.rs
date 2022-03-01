d28a5eb18c76296d2cc4053b33e26c5c409a395d
rusgot <raymondcasy@gmail.com>
2022-03-01 15:22:10 -0600

    More workspace stuff

---
+++ projects/add/add-one/src/lib.rs
@@ -0,0 +1,15 @@
+use rand;
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_one_check() {
+        assert_eq!(add_one(3), 4);
+    }
+}
---
+++ projects/add/adder/src/main.rs
@@ -0,0 +1,17 @@
+use add_one;
+use add_two;
+
+fn main() {
+    let num = 10;
+    println!(
+        "Hello world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+
+    println!(
+        "Also, {} plus two is {}!",
+        num,
+        add_two::add_two(num)
+    );
+}
